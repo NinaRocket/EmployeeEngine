de7b5c128157bc97f712abe3889ae9eea6bc9ed6
NRocket <ninamrocket@gmail.com>
2020-06-14 12:36:02 -0700

    validation added

--- Develop/app.js
+++ Develop/app.js
@@ -12,9 +12,7 @@ const render = require("./lib/htmlRenderer");
 
 //array to push added employees into
 const employeeArr = [];
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+//function to add employee, if/else statements to direct to function of employee role
 function addEmployee() {
     return inquirer.prompt([
         {
@@ -44,19 +42,37 @@ function addEmployee() {
         }
     })
 };
-
+//function to add a new engineer
 function newEngineer() {
     return inquirer.prompt([
 
         {
             type: "input",
             name: "name",
-            message: "Please enter the name of your engineer?"
+            message: "Please enter the name of your engineer?",
+            validate: name => {
+                if (!name.length) {
+                    console.log("\nPlease enter a name.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
             name: "id",
-            message: "Please enter the engineer's ID number"
+            message: "Please enter the engineer's ID number",
+            validate: id => {
+                if (isNaN(parseInt(id))) {
+                    console.log("\nEmployee id should be a number");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+
         },
         {
             type: "input",
@@ -66,7 +82,16 @@ function newEngineer() {
         {
             type: "input",
             name: "github",
-            message: "Please enter the engineer's github username."
+            message: "Please enter the engineer's github username.",
+            validate: username => {
+                if (!username.length) {
+                    console.log("\nPlease enter a name");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
         }
     ]).then(data => {
         console.log("An engineer was added.");
@@ -75,28 +100,63 @@ function newEngineer() {
         addEmployee();
     })
 };
-
+//function to add new intern
 function newIntern() {
     return inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "Please enter the name of your intern?"
+            message: "Please enter the name of your intern?",
+            validate: name => {
+                if (!name.length) {
+                    console.log("\nPlease enter a name.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
             name: "id",
-            message: "Please enter the intern's ID number"
+            message: "Please enter the intern's ID number",
+            validate: id => {
+                if (isNaN(parseInt(id))) {
+                    console.log("\nEmployee id should be a number");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
             name: "email",
-            message: "Please enter the intern's email address."
+            message: "Please enter the intern's email address.",
+            validate: email => {
+                const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter a valid email")
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "school",
-            message: "Please enter the intern's school name."
+            message: "Please enter the intern's school name.",
+            validate: school => {
+                if (!school.length) {
+                    console.log("\nPlease enter a school.");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
         }
     ]).then(data => {
         console.log("An intern was added.");
@@ -105,32 +165,65 @@ function newIntern() {
         addEmployee();
     })
 };
-
+//function to add new manager
 function newManager() {
-
     return inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "Please enter the Manager's name."
+            message: "Please enter the Manager's name.",
+            validate: name => {
+                if (!name.length) {
+                    console.log("\nPlease enter a name.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
             name: "id",
-            message: "Please enter the Manager's ID number"
+            message: "Please enter the Manager's ID number",
+            validate: id => {
+                if (isNaN(parseInt(id))) {
+                    console.log("\nEmployee id should be a number");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
         },
         {
             type: "input",
             name: "email",
-            message: "Please enter the Manager's email address."
+            message: "Please enter the Manager's email address.",
+            validate: email => {
+                const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter a valid email")
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "officeNumber",
-            message: "Please enter the manager's office number."
+            message: "Please enter the manager's office number.",
+            validate: num => {
+                if (isNaN(parseInt(num))) {
+                    console.log("\nEmployee id should be a number");
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
         }
     ]).then(data => {
-        const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email);
         console.log("A manager was added.");
         let manager = new Manager(data.name, data.id, data.email, data.officeNumber);
         employeeArr.push(manager);
@@ -139,23 +232,4 @@ function newManager() {
 };
 
 addEmployee();
-//console.log(employeeArr);
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ``
